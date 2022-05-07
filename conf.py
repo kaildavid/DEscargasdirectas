b7a0ebfb369ab90d775c216f4d4ca55198a16b41
Jose 752 <93238024+jose752jose@users.noreply.github.com>
2022-05-07 10:00:15 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-api_hash = "apihash"
-api_id = 1736372819
-bot_token = "bottoken"
+api_hash = "2c0b01283b1b67f7087a92cc81cd91f8"
+api_id = 18403498
+bot_token = "5312082582:AAFJVuKU9y1Mul2sJpFLhlJzKH3cdib0wlk"
 authorized_users = []
-admin_user = 'admindelbot'
+admin_user = 'JOSE_752'
