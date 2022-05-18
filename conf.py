1a3488461020c87a5cc585c50b9f3b9842d2c637
Jose 752 <93238024+jose752jose@users.noreply.github.com>
2022-05-18 01:55:12 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "2c0b01283b1b67f7087a92cc81cd91f8"
 api_id = 18403498
 bot_token = "5312082582:AAFJVuKU9y1Mul2sJpFLhlJzKH3cdib0wlk"
-authorized_users = ['FACKIU','trexx04diego','henry9510','valdes_95']
+authorized_users = ['FACKIU','trexx04diego','henry9510','valdes_95','aki_casual']
 admin_user = 'JOSE_752'
